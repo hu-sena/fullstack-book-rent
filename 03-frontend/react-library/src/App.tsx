e090ba76c6cf69cf16d47471ee3f7a1327ee05b7
Husna Zubir <shusnazubir@gmail.com>
2023-12-29 15:33:59 +0800

    add heros component

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './Layouts/Homepage/ExploreTopBooks';
 import { Carousel } from './Layouts/Homepage/Carousel';
+import { Heros } from './Layouts/Homepage/Heros';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <ExploreTopBooks/>
       <Carousel/>
+      <Heros/>
     </div>
 
   );
