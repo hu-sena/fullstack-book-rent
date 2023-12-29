15b18a7ab61c4c39ae7125cd8745124892eedc96
Husna Zubir <shusnazubir@gmail.com>
2023-12-29 14:02:56 +0800

    resolve conflict on exporting

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
