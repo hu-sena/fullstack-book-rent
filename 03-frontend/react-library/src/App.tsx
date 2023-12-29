5b1e37d17d9b5ca8fc6dd6ffdd78a8fff5d6d4f1
Husna Zubir <shusnazubir@gmail.com>
2023-12-29 14:58:59 +0800

    add carousel on homepage

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './Layouts/Homepage/ExploreTopBooks';
+import { Carousel } from './Layouts/Homepage/Carousel';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <ExploreTopBooks/>
+      <Carousel/>
     </div>
 
   );
