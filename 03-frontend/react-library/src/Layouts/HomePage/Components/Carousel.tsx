b5f60132deec00fa921a86898471bb319929abb0
Husna Zubir <shusnazubir@gmail.com>
2024-01-01 21:26:11 +0800

    add ReturnBook props to display data dynamically from backend + add loading spinner

--- 03-frontend/react-library/src/Layouts/HomePage/Components/Carousel.tsx
+++ 03-frontend/react-library/src/Layouts/HomePage/Components/Carousel.tsx
@@ -2,6 +2,7 @@ import { error } from "console";
 import BookModel from "../../../Models/BookModels";
 import { ReturnBook } from "./ReturnBook";
 import { useEffect, useState } from "react";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const Carousel = () => {
 
@@ -49,9 +50,7 @@ export const Carousel = () => {
 
     if (isLoading) {
         return (
-            <div className='container m-5'>
-                <p>Laoding ...</p>
-            </div>
+            <SpinnerLoading />
         )
     }
 
@@ -64,56 +63,56 @@ export const Carousel = () => {
         )
     }
 
-        return (
-            <div className='container mt-5' style={{ height: 550 }}>
-                <div className='homepage-carousel-title'>
-                    <h3>Find your next "I stayed up too late reading" book.</h3>
-                </div>
-                <div className='carousel carousel-dark slide mt-5 d-none d-lg-block' id='carouselExampleControls' data-bs-interval='false'>
+    return (
+        <div className='container mt-5' style={{ height: 550 }}>
+            <div className='homepage-carousel-title'>
+                <h3>Find your next "I stayed up too late reading" book.</h3>
+            </div>
+            <div className='carousel carousel-dark slide mt-5 d-none d-lg-block' id='carouselExampleControls' data-bs-interval='false'>
 
-                    {/* Desktop */}
-                    <div className='carousel-inner'>
-                        <div className='carousel-item active'>
-                            <div className='row d-flex justify-content-center align-items-center'>
-                                <ReturnBook />
-                                <ReturnBook />
-                                <ReturnBook />
-                            </div>
+                {/* Desktop */}
+                <div className='carousel-inner'>
+                    <div className='carousel-item active'>
+                        <div className='row d-flex justify-content-center align-items-center'>
+                            {books.slice(0, 3).map(book => (
+                                <ReturnBook book={book} key={book.id} />
+                            ))}
                         </div>
-                        <div className='carousel-item'>
-                            <div className='row d-flex justify-content-center align-items-center'>
-                                <ReturnBook />
-                                <ReturnBook />
-                                <ReturnBook />
-                            </div>
-                        </div>
-                        <div className='carousel-item'>
-                            <div className='row d-flex justify-content-center align-items-center'>
-                                <ReturnBook />
-                                <ReturnBook />
-                                <ReturnBook />
-                            </div>
-                        </div>
-                        <button className='carousel-control-prev' type='button' data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
-                            <span className='carousel-control-prev-icon' aria-hidden='true'></span>
-                            <span className='visually-hidden'>Previous</span>
-                        </button>
-                        <button className='carousel-control-next' type='button' data-bs-target='#carouselExampleControls' data-bs-slide='next'>
-                            <span className='carousel-control-next-icon' aria-hidden='true'></span>
-                            <span className='visually-hidden'>Next</span>
-                        </button>
                     </div>
-                </div>
-
-                {/* Mobile */}
-                <div className='d-lg-none mt-3'>
-                    <div className='row d-flex justify-content-center align-items-center'>
-                        <ReturnBook />
+                    <div className='carousel-item'>
+                        <div className='row d-flex justify-content-center align-items-center'>
+                            {books.slice(3, 6).map(book => (
+                                <ReturnBook book={book} key={book.id} />
+                            ))}
+                        </div>
                     </div>
-                </div>
-                <div className='homepage-carousel-title mt-3'>
-                    <a className='btn btn-outline-secondary btn-lg' href='#'>View More</a>
+                    <div className='carousel-item'>
+                        <div className='row d-flex justify-content-center align-items-center'>
+                            {books.slice(6, 9).map(book => (
+                                <ReturnBook book={book} key={book.id} />
+                            ))}
+                        </div>
+                    </div>
+                    <button className='carousel-control-prev' type='button' data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+                        <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+                        <span className='visually-hidden'>Previous</span>
+                    </button>
+                    <button className='carousel-control-next' type='button' data-bs-target='#carouselExampleControls' data-bs-slide='next'>
+                        <span className='carousel-control-next-icon' aria-hidden='true'></span>
+                        <span className='visually-hidden'>Next</span>
+                    </button>
                 </div>
             </div>
-        );
+
+            {/* Mobile */}
+            <div className='d-lg-none mt-3'>
+                <div className='row d-flex justify-content-center align-items-center'>
+                    <ReturnBook book={books[7]} key={books[7].id} />
+                </div>
+            </div>
+            <div className='homepage-carousel-title mt-3'>
+                <a className='btn btn-outline-secondary btn-lg' href='#'>View More</a>
+            </div>
+        </div>
+    );
 }
