3932de94f34194fd3c598485f913faff5a9481d5
Husna Zubir <shusnazubir@gmail.com>
2024-01-02 18:16:03 +0800

    add Switch and Route for each component

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,20 +4,23 @@ import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
 import { Footer } from './Layouts/NavbarAndFooter/Footer';
 import { HomePage } from './Layouts/HomePage/HomePage';
 import { SearchBooksPage } from './Layouts/SearchBooksPage/SearchBooksPage';
-import { Route } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <Route path='/'>
-        <HomePage />
-      </Route>
-      
-      <Route path='/search'>
-        <SearchBooksPage />
-      </Route>
-
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home' />
+        </Route>
+        <Route path='/home'>
+          <HomePage />
+        </Route>
+        <Route path='/search'>
+          <SearchBooksPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
 
