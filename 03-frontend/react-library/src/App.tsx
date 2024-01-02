831bdba0a6ffdb2468d80f6db572b3997da06478
Husna Zubir <shusnazubir@gmail.com>
2024-01-02 18:07:42 +0800

    install react router

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,13 +4,20 @@ import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
 import { Footer } from './Layouts/NavbarAndFooter/Footer';
 import { HomePage } from './Layouts/HomePage/HomePage';
 import { SearchBooksPage } from './Layouts/SearchBooksPage/SearchBooksPage';
+import { Route } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <HomePage/> */}
-      <SearchBooksPage />
+      <Route path='/'>
+        <HomePage />
+      </Route>
+      
+      <Route path='/search'>
+        <SearchBooksPage />
+      </Route>
+
       <Footer />
     </div>
 
