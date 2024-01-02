5b4a14b982e767739fb8f1da923e4b70944f17ec
Husna Zubir <shusnazubir@gmail.com>
2024-01-02 15:35:28 +0800

    Spring Boot - Find By Title

--- 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/dao/BookRepository.java
@@ -1,7 +1,18 @@
 package com.springbootapp.springbootlibrary.dao;
 
 import com.springbootapp.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository <Book, Long> {
+
+//    Page: hold paginated data, Pageable: specify pagination options
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable paginateOptions);
+
+//      implemented from method name -- Spring Data JPA detect from naming convention to query database
+//        "search": {
+//            "href": "http://localhost:8080/api/books/search"
+//        }
 }
