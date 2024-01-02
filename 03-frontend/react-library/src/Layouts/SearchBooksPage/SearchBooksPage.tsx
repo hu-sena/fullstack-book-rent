55b590cacb2829c4c0788226bc80c52dbb6fbb7b
Husna Zubir <shusnazubir@gmail.com>
2024-01-02 16:42:45 +0800

    solve issue if totalAmountOfBooks is 0 in search result

--- 03-frontend/react-library/src/Layouts/SearchBooksPage/SearchBooksPage.tsx
+++ 03-frontend/react-library/src/Layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -110,7 +110,7 @@ export const SearchBooksPage = () => {
                                     placeholder='Search' aria-labelledby='Search'
                                     onChange={e => setSearch(e.target.value)} />
                                 <button className='btn btn-outline-success'
-                                onClick={() => searchHandleChange()}>
+                                    onClick={() => searchHandleChange()}>
                                     Search
                                 </button>
                             </div>
@@ -152,15 +152,27 @@ export const SearchBooksPage = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='mt-3'>
-                        <h5>Number of results: ({totalAmountOfBooks})</h5>
-                    </div>
-                    <p>
-                        {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
-                    </p>
-                    {books.map(book => (
-                        <SearchBook book={book} key={book.id} />
-                    ))}
+                    {totalAmountOfBooks > 0 ?
+                        <>
+                            <div className='mt-3'>
+                                <h5>Number of results: ({totalAmountOfBooks})</h5>
+                            </div>
+                            <p>
+                                {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
+                            </p>
+                            {books.map(book => (
+                                <SearchBook book={book} key={book.id} />
+                            ))}
+                        </>
+                        :
+                        <div className='m-5'>
+                            <h3>
+                                Can't find what you are looking for?
+                            </h3>
+                            <a type='button' className='btn main-color btn-md px-4 me-md-2 fw-bold text-white'
+                                href='#'>Library Services</a>
+                        </div>
+                    }
                     {totalPages > 1 &&
                         <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate} />}
                 </div>
