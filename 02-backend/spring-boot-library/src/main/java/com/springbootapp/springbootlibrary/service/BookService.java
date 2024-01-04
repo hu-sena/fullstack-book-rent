67b282a090758631477da7efba818c3a085c70b0
Husna Zubir <shusnazubir@gmail.com>
2024-01-04 23:32:32 +0800

    add BookService for checkout functionality

---
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/service/BookService.java
@@ -0,0 +1,52 @@
+package com.springbootapp.springbootlibrary.service;
+
+import com.springbootapp.springbootlibrary.dao.BookRepository;
+import com.springbootapp.springbootlibrary.dao.CheckoutRepository;
+import com.springbootapp.springbootlibrary.entity.Book;
+import com.springbootapp.springbootlibrary.entity.Checkout;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class BookService {
+
+    private BookRepository bookRepository;
+    private CheckoutRepository checkoutRepository;
+
+//    dependency injection
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository) {
+        this.bookRepository = bookRepository;
+        this.checkoutRepository = checkoutRepository;
+    }
+
+    public Book checkoutBook(String userEmail, Long bookId) throws Exception {
+
+//        bookId can be present absent (null) or present
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if(!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0) {
+            throw new Exception("Book doesn't exist or already checkout by other user");
+
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
+        bookRepository.save(book.get());
+
+        Checkout checkout = new Checkout(
+                userEmail,
+                LocalDate.now().toString(),
+                LocalDate.now().plusDays(7).toString(),
+                book.get().getId()
+        );
+
+        checkoutRepository.save(checkout);
+
+        return book.get();
+    }
+}
