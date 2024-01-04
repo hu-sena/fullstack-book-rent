4173610a6b7bf6af98b1710d22bb96f982a07042
Husna Zubir <shusnazubir@gmail.com>
2024-01-04 13:09:20 +0800

    add configuration to re-route login page

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,10 +4,29 @@ import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
 import { Footer } from './Layouts/NavbarAndFooter/Footer';
 import { HomePage } from './Layouts/HomePage/HomePage';
 import { SearchBooksPage } from './Layouts/SearchBooksPage/SearchBooksPage';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import { BookCheckoutPage } from './Layouts/BookCheckoutPage/BookCheckoutPage';
+import { oktaConfig } from './Lib/oktaConfig';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
+
+  const history = useHistory();
+
+  const customAuthHandler = () => {
+    history.push('login')
+  }
+
+  // replace the current URL (toRelativeUrl) with originalUri (authentication)
+  //  - thten navigating back to the inteded page after authenticated
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin))
+  }
+
+
+
   return (
     <div className='d-flex flex-column min-vh-100'>
       <Navbar />
