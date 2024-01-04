eb6bab54b4607162a5876fa2bbb4dbe9d81e46dc
Husna Zubir <shusnazubir@gmail.com>
2024-01-04 22:17:52 +0800

    refactor all buttons when authenticated

--- 03-frontend/react-library/src/Layouts/HomePage/Components/Heros.tsx
+++ 03-frontend/react-library/src/Layouts/HomePage/Components/Heros.tsx
@@ -1,4 +1,10 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { Link } from "react-router-dom";
+
 export const Heros = () => {
+
+    const { authState } = useOktaAuth();
+
     return (
         <div>
             <div className='d-none d-lg-block'>
@@ -14,7 +20,12 @@ export const Heros = () => {
                                 Whether it is to learn a new skill or grow within one,
                                 we will be able to provide the top content for you!
                             </p>
-                            <a className='btn main-color btn-lg text-white' href='#'>Sign Up</a>
+                            {authState?.isAuthenticated ?
+                                <Link className='btn main-color btn-lg text-white' type='button' to='search'>Explore top books</Link>
+                                :
+                                <Link className='btn main-color btn-lg text-white' to='/login'>Sign Up</Link>
+                            }
+
                         </div>
                     </div>
                 </div>
@@ -49,7 +60,11 @@ export const Heros = () => {
                                 Whether it is to learn a new skill or grow within one,
                                 we will be able to provide the top content for you!
                             </p>
-                            <a className='btn main-color btn-lg text-white' href='#'>Sign up</a>
+                            {authState?.isAuthenticated ?
+                                <Link className='btn main-color btn-lg text-white' type='button' to='/search'>Explore top books</Link>
+                                :
+                                <Link className='btn main-color btn-lg text-white' to='/login'>Sign Up</Link>
+                            }
                         </div>
                     </div>
                     <div className='m-2'>
