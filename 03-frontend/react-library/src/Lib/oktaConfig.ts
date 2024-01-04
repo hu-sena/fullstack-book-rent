fe1dcdbefd86790426234e4ead1d88b8f74deaac
Husna Zubir <shusnazubir@gmail.com>
2024-01-04 14:01:42 +0800

    solve issue on unable to redirect after login

--- 03-frontend/react-library/src/Lib/oktaConfig.ts
+++ 03-frontend/react-library/src/Lib/oktaConfig.ts
@@ -2,7 +2,7 @@ export const oktaConfig = {
     clientId: '0oae95f5uoHGFB9eN5d7',
     issuer: 'https://dev-96824086.okta.com/oauth2/default',
     redirectUri: 'http://localhost:3000/login/callback',
-    scopes: ['opendid', 'profile', 'email'],
+    scope: ['opendid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
 }
