ecc2e7f5cd11b2d0683af814d2b3d900646a111f
Husna Zubir <shusnazubir@gmail.com>
2024-01-05 02:04:58 +0800

    add SecurityConfiguration to secure backend

---
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.springbootapp.springbootlibrary.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+//        disable Cross Site Request Forgery
+        http.csrf(AbstractHttpConfigurer::disable);
+
+//        protect endpoints at /api/<type>/secure
+        http.authorizeRequests(configurer ->
+                configurer
+                        .antMatchers("/api/books/secure/**")
+                        .authenticated().anyRequest().permitAll())
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(Customizer.withDefaults()));
+
+//        add CORS filters
+        http.cors(Customizer.withDefaults());
+
+//        add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+
+//        force a non-empty response body for 401's to make the response friendly
+        Okta.configureResourceServer401ResponseBody(http);
+
+        return http.build();
+    }
+}
