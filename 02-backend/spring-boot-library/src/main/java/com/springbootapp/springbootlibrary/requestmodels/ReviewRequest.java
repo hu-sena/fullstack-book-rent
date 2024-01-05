fc016187eee864effe3fddf5482ce7a06d6f50be
Husna Zubir <shusnazubir@gmail.com>
2024-01-05 23:38:51 +0800

    create a request model for ReviewRequest for user to add a review

---
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/requestmodels/ReviewRequest.java
@@ -0,0 +1,12 @@
+package com.springbootapp.springbootlibrary.requestmodels;
+
+import lombok.Data;
+import java.util.Optional;
+
+@Data
+public class ReviewRequest {
+
+    private double rating;
+    private long bookId;
+    private Optional<String> reviewDescription;
+}
