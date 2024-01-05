566d5244c1940e0a52a1e4f0cf2720bd148d2dce
Husna Zubir <shusnazubir@gmail.com>
2024-01-06 01:53:02 +0800

    add reviewRender to show message in CheckoutAndReviewBox

--- 03-frontend/react-library/src/Layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/Layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -142,7 +142,7 @@ export const BookCheckoutPage = () => {
                 }
 
                 const responseJsonUserReview = await responseUserReview.json();
-                setCurrentLoansCount(responseJsonUserReview);
+                setIsReviewLeft(responseJsonUserReview);
 
             }
             setIsLoadingUserReview(false);
@@ -283,7 +283,7 @@ export const BookCheckoutPage = () => {
                     </div>
                     <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount}
                         isAuthenticated={authState?.isAuthenticated} isCheckedOut={isBookCheckedOut}
-                        checkoutBook={checkoutBook} />
+                        checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} />
                 </div>
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
@@ -308,7 +308,7 @@ export const BookCheckoutPage = () => {
                 </div>
                 <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount}
                     isAuthenticated={authState?.isAuthenticated} isCheckedOut={isBookCheckedOut}
-                    checkoutBook={checkoutBook} />
+                    checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} />
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
             </div>
