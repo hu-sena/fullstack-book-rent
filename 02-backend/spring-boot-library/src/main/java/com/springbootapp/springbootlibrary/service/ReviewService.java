97e23584538a28cab3697925a1da94706e57f907
Husna Zubir <shusnazubir@gmail.com>
2024-01-05 23:59:08 +0800

    create ReviewService to add (or POST request) user's review into database

---
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/service/ReviewService.java
@@ -0,0 +1,51 @@
+package com.springbootapp.springbootlibrary.service;
+
+import com.springbootapp.springbootlibrary.dao.BookRepository;
+import com.springbootapp.springbootlibrary.dao.ReviewRepository;
+import com.springbootapp.springbootlibrary.entity.Review;
+import com.springbootapp.springbootlibrary.requestmodels.ReviewRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Service
+@Transactional
+public class ReviewService {
+
+    private BookRepository bookRepository;
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(BookRepository bookRepository, ReviewRepository reviewRepository) {
+        this.bookRepository = bookRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    public void postReview(String userEmail, ReviewRequest reviewRequest) throws Exception {
+        Review validateReview = reviewRepository.findByUserEmailAndBookId(userEmail, reviewRequest.getBookId());
+
+        if (validateReview != null) {
+            throw new Exception("Review already been created");
+        }
+
+        Review review = new Review();
+
+        review.setBookId(reviewRequest.getBookId());
+        review.setRating(reviewRequest.getRating());
+        review.setUserEmail(userEmail);
+
+        if (reviewRequest.getReviewDescription().isPresent()) {
+            review.setReviewDescription(reviewRequest.getReviewDescription().map(
+//                    convert object from Optional<> to string
+                    Object::toString
+            ).orElse(null));
+        }
+
+        review.setDate(Date.valueOf(LocalDate.now()));
+        reviewRepository.save(review);
+
+    }
+}
