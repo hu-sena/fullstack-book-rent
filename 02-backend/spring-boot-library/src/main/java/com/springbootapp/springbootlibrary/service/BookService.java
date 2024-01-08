97f7f61737a57313d61a558b78057e55cf8ecae2
Husna Zubir <shusnazubir@gmail.com>
2024-01-09 00:26:59 +0800

    add renewLoan method in BookService

--- 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/service/BookService.java
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/service/BookService.java
@@ -96,7 +96,7 @@ public class BookService {
                     .filter(x -> x.getBookId() == book.getId()).findFirst();
 
             if (checkout.isPresent()) {
-//                  get(): method to access the elements
+//                  get(): method to access the elements that has value
                 Date returnDate = dateFormat.parse(checkout.get().getReturnDate());
                 Date currentDate = dateFormat.parse(LocalDate.now().toString());
 
@@ -122,11 +122,30 @@ public class BookService {
             throw new Exception("Book does not exist or not checked out by user");
         }
 
-//        using get() method: if the value is present
+//        get(): method to access the elements that has value (present)
         book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
 
         bookRepository.save(book.get());
         checkoutRepository.deleteById(vaildateCheckout.getId());
 
+    }
+
+    public void renewLoan(String userEmail, Long bookId) throws Exception {
+
+        Checkout vaildateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+        if(vaildateCheckout == null) {
+            throw new Exception("Book does not exist or not checked out by user");
+        }
+
+//        allow only if it is not past the loan period
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date returnDate = dateFormat.parse(vaildateCheckout.getReturnDate());
+        Date currentDate = dateFormat.parse(LocalDate.now().toString());
+
+        if (returnDate.compareTo(currentDate) > 0 || returnDate.compareTo(currentDate) == 0) {
+            vaildateCheckout.setReturnDate(LocalDate.now().plusDays(7).toString());
+            checkoutRepository.save(vaildateCheckout);
+        }
     }
 }
