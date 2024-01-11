3a05093b3934cd711cb02ae9b910fad6e5c3a0b4
Husna Zubir <shusnazubir@gmail.com>
2024-01-12 00:22:56 +0800

    add link to LibraryService component in HomePage

--- 03-frontend/react-library/src/Layouts/HomePage/Components/LibraryServices.tsx
+++ 03-frontend/react-library/src/Layouts/HomePage/Components/LibraryServices.tsx
@@ -18,9 +18,11 @@ export const LibraryServices = () => {
                     </p>
                     <div className='d-grid gap-2 justify-content-md-start mb-4 mb-lg-3'>
                         {authState?.isAuthenticated ?
-                            <Link className='btn main-color btn-lg px-4 me-md-2 fw-bold text-white' type='button' to='#'>Library Services</Link>
+                            <Link className='btn main-color btn-lg px-4 me-md-2 fw-bold text-white' type='button' to='/messages'>
+                                Library Services</Link>
                             :
-                            <Link className='btn main-color btn-lg text-white' to='/login'>Sign Up</Link>
+                            <Link className='btn main-color btn-lg text-white' to='/login'>
+                                Sign Up</Link>
                         }
                     </div>
                 </div>
