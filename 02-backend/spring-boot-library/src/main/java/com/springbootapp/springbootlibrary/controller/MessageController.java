f1e6a43139e7fbe8fbf8b604e7bd3e0de29a40c8
Husna Zubir <shusnazubir@gmail.com>
2024-01-11 22:29:51 +0800

    add POST request for user to post message

---
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/controller/MessageController.java
@@ -0,0 +1,26 @@
+package com.springbootapp.springbootlibrary.controller;
+
+import com.springbootapp.springbootlibrary.entity.Message;
+import com.springbootapp.springbootlibrary.service.MessageService;
+import com.springbootapp.springbootlibrary.util.ExtractJWT;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    private MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value = "Authorization") String token,
+                            @RequestBody Message messageRequest) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messageService.postMessage(messageRequest, userEmail);
+
+    }
+}
