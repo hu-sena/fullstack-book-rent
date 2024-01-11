c4942304fddfe154351cffd4f99e461c59ea4766
Husna Zubir <shusnazubir@gmail.com>
2024-01-11 23:19:52 +0800

    create PostNewMessage component

--- 03-frontend/react-library/src/Layouts/MessagesPage/MessagesPage.tsx
+++ 03-frontend/react-library/src/Layouts/MessagesPage/MessagesPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { PostNewMessage } from './Components/PostNewMessage';
 
 export const MessagesPage = () => {
 
@@ -23,7 +24,7 @@ export const MessagesPage = () => {
                 </nav>
                 <div className='tab-content' id='nav-tabContent'>
                     <div className='tab-pane fade show active' id='nav-send-message' role='tabpanel' aria-labelledby='nav-send-message-tab'>
-                        <p>Post new message</p>
+                        <PostNewMessage />
                     </div>
                     <div className='tab-pane fade' id='nav-message' role='tabpanel' aria-labelledby='nav-message-tab'>
                         {messagesClick ? <p>Messages</p> : <></>}
