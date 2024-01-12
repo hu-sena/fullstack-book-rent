a0356f15ce01905ebcb5d353ea0d31bfcc2776d7
Husna Zubir <shusnazubir@gmail.com>
2024-01-12 23:05:42 +0800

    add putMessage endpoint of PUT request in MessageController

--- 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/controller/MessageController.java
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.springbootapp.springbootlibrary.controller;
 
 import com.springbootapp.springbootlibrary.entity.Message;
+import com.springbootapp.springbootlibrary.requestmodels.AdminQuestionRequest;
 import com.springbootapp.springbootlibrary.service.MessageService;
 import com.springbootapp.springbootlibrary.util.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,20 @@ public class MessageController {
                             @RequestBody Message messageRequest) {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messageService.postMessage(messageRequest, userEmail);
+
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+
+        messageService.putMessage(adminQuestionRequest, userEmail);
 
     }
 }
