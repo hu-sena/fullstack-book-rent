72b00b7df676627bd26b452e5ffd653037ca83a3
Husna Zubir <shusnazubir@gmail.com>
2024-01-12 23:58:41 +0800

    create AdminController + an endpoint of POST request for postBook

---
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/controller/AdminController.java
@@ -0,0 +1,34 @@
+package com.springbootapp.springbootlibrary.controller;
+
+
+import com.springbootapp.springbootlibrary.requestmodels.AddBookRequest;
+import com.springbootapp.springbootlibrary.service.AdminService;
+import com.springbootapp.springbootlibrary.util.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value="Authorization") String token,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.postBook(addBookRequest);
+    }
+
+
+}
