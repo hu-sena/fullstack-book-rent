f820b1895a3255ec32abe86a90b3a5b2380a4683
Husna Zubir <shusnazubir@gmail.com>
2024-01-12 22:57:56 +0800

    add putMessage method in MessageService

--- 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/service/MessageService.java
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/service/MessageService.java
@@ -2,9 +2,12 @@ package com.springbootapp.springbootlibrary.service;
 
 import com.springbootapp.springbootlibrary.dao.MessageRepository;
 import com.springbootapp.springbootlibrary.entity.Message;
+import com.springbootapp.springbootlibrary.requestmodels.AdminQuestionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,9 +20,25 @@ public class MessageService {
         this.messageRepository = messageRepository;
     }
 
+//    user create a message
     public void postMessage(Message messageRequest, String userEmail) {
             Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
             message.setUserEmail(userEmail);
             messageRepository.save(message);
+    }
+
+//    admin update the message
+    public void putMessage(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception {
+        Optional<Message> message = messageRepository.findById(adminQuestionRequest.getId());
+
+        if (!message.isPresent()) {
+           throw new Exception("Message not found");
+        }
+
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+
+        messageRepository.save(message.get());
     }
 }
