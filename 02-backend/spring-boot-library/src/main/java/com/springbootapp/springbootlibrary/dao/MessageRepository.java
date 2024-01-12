0877ae5efc5e15e85dc55554d75fb008a2baa169
Husna Zubir <shusnazubir@gmail.com>
2024-01-12 21:56:53 +0800

    add an endpoint to DAO to show all unanswered questions from all users

--- 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/dao/MessageRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/dao/MessageRepository.java
@@ -4,10 +4,14 @@ import com.springbootapp.springbootlibrary.entity.Message;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
 
     Page<Message> findByUserEmail(@RequestParam("user_email") String userEmail, Pageable paginateOptions);
+
+//    admin: only see unanswered questions for all users
+    Page<Message> findByClosed(@RequestParam("closed") boolean closed, Pageable paginateOptions);
 }
