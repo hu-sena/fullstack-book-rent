cb41cc6e3cea0213a446eb2313c3a1de4a7723d3
Husna Zubir <shusnazubir@gmail.com>
2024-01-13 22:51:57 +0800

    update URI to HTTPS

--- 03-frontend/react-library/src/Lib/oktaConfig.ts
+++ 03-frontend/react-library/src/Lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oae95f5uoHGFB9eN5d7',
     issuer: 'https://dev-96824086.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scope: ['opendid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
