e2c9ea15bc6679e7cabd26e393407e48cf5efcdc
Husna Zubir <shusnazubir@gmail.com>
2024-01-13 16:31:30 +0800

    create AddNewBook component

--- 03-frontend/react-library/src/Layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ 03-frontend/react-library/src/Layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -2,6 +2,7 @@ import { useOktaAuth } from "@okta/okta-react"
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { AdminMessages } from "./Components/AdminMessages";
+import { AddNewBook } from "./Components/AddNewBook";
 
 export const ManageLibraryPage = () => {
 
@@ -56,7 +57,7 @@ export const ManageLibraryPage = () => {
                 <div className='tab-content' id='nav-tabContent'>
                     <div className='tab-pane fade show active' id='nav-add-book' role='tabpanel'
                         aria-labelledby='nav-add-book-tab'>
-                        Add new book
+                        <AddNewBook />
                     </div>
                     <div className='tab-pane fade' id='nav-quantity' role='tabpanel'
                         aria-labelledby='nav-quantity-tab'>
