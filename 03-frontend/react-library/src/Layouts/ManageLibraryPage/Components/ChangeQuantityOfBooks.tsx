b24e221be281edfd22cd8859fcc36304c01b6ec6
Husna Zubir <shusnazubir@gmail.com>
2024-01-13 18:04:43 +0800

    create ChangeQuantityOfBook for child component to display each book

--- 03-frontend/react-library/src/Layouts/ManageLibraryPage/Components/ChangeQuantityOfBooks.tsx
+++ 03-frontend/react-library/src/Layouts/ManageLibraryPage/Components/ChangeQuantityOfBooks.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BookModel from "../../../Models/BookModels";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 import { Pagination } from "../../Utils/Pagination";
+import { ChangeQuantityOfBook } from "./ChangeQuantityOfBook";
 
 export const ChangeQuantityOfBooks = () => {
 
@@ -90,7 +91,7 @@ export const ChangeQuantityOfBooks = () => {
                         {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
                     </p>
                     {books.map(book => (
-                        <p>Display different quantity of books</p>
+                        <ChangeQuantityOfBook book={book} key={book.id} />
                     ))}
                 </>
                 :
