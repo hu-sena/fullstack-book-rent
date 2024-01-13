83200c0a1a3ecc9bfe8e22f4774af41bcb1f77df
Husna Zubir <shusnazubir@gmail.com>
2024-01-13 18:32:48 +0800

    add increaseBookQuantity method in AdminService

--- 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/service/AdminService.java
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/service/AdminService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class AdminService {
@@ -18,6 +20,19 @@ public class AdminService {
         this.bookRepository = bookRepository;
     }
 
+    public void increaseBookQuantity(Long bookId) throws Exception {
+//        retrieve all books based on ID into book object
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if (!book.isPresent()) {
+            throw new Exception("Book not found");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() +1);
+        book.get().setCopies(book.get().getCopies() + 1);
+
+        bookRepository.save(book.get());
+    }
     public void postBook(AddBookRequest addBookRequest) {
         Book book = new Book();
         book.setTitle(addBookRequest.getTitle());
