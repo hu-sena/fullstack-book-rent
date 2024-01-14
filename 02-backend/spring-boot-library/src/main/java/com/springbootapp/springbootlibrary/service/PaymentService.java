b4562eb780c3f59036d7c744c5b5a435a3d8a57f
Husna Zubir <shusnazubir@gmail.com>
2024-01-14 19:24:30 +0800

    create PaymentService

---
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/service/PaymentService.java
@@ -0,0 +1,19 @@
+package com.springbootapp.springbootlibrary.service;
+
+import com.springbootapp.springbootlibrary.dao.PaymentRepository;
+import com.stripe.Stripe;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PaymentService {
+
+    private PaymentRepository paymentRepository;
+
+    public PaymentService(PaymentRepository paymentRepository, @Value("${stripe.key.secret}") String secretKey) {
+        this.paymentRepository = paymentRepository;
+        Stripe.apiKey = secretKey;
+    }
+}
