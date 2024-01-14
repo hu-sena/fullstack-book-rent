491a1d5f8056812748a24ed1b0c6f5108414fcfd
Husna Zubir <shusnazubir@gmail.com>
2024-01-14 20:26:06 +0800

    create PaymentController + endpoints for payment-intent and payment-complete

---
+++ 02-backend/spring-boot-library/src/main/java/com/springbootapp/springbootlibrary/controller/PaymentController.java
@@ -0,0 +1,46 @@
+package com.springbootapp.springbootlibrary.controller;
+
+import com.springbootapp.springbootlibrary.requestmodels.PaymentInfoRequest;
+import com.springbootapp.springbootlibrary.service.PaymentService;
+import com.springbootapp.springbootlibrary.util.ExtractJWT;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("https://localhost:3000")
+@RestController
+@RequestMapping("/api/payment/secure")
+public class PaymentController {
+
+    private PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+//    API provided by stripe
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfoRequest paymentInfoRequest)
+                                                      throws StripeException {
+
+        PaymentIntent paymentIntent = paymentService.createPaymentIntent(paymentInfoRequest);
+//        convert to JSON format
+        String paymentString = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentString, HttpStatus.OK);
+    }
+
+    @PutMapping("/payment-complete")
+    public ResponseEntity<String> stripePaymentComplete(@RequestHeader(value="Authorization") String token) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+
+        return paymentService.stripePayment(userEmail);
+
+    }
+}
